0063e0c1284a14b17ab40b40ec5d8713c48b5eb8
OrangeLube <38672170+OrangeLube@users.noreply.github.com>
2018-04-24 18:51:46 -0400

    Reorganized the command classes, updated help to include for info on command, reformated help, limited meme classes to only be able to post in #memes

--- ConsoleApp1/Modules/Admin/help.cs
+++ ConsoleApp1/Modules/Admin/help.cs
@@ -15,19 +15,14 @@ namespace ConsoleApp1.Modules
         {
             //Create Embed Builder
             EmbedBuilder builder = new EmbedBuilder();
-            builder.WithTitle("OrangeBot Help")                 //Set Title
-                .WithDescription("!add_role [role name]\n" +    //Set Description
-                                 "!cat\n" +
-                                 "!compliment\n" +
-                                 "!insult\n" +
-                                 "!remove_role [role name]\n" + 
-                                 "!quote\n" + 
-                                 "!respectcounter\n" +
-                                 "!surrealmeme\n" +
-                                 "!dankmeme\n" +
-                                 "!help [command]\n")
+            builder.AddField("OrangeBot Help", "This is the help hot line for help with OrangeBot Commands.  Type !help [command] for more information on certain commands")        //Set Title
+                   .AddField("Role Commands", "!add_role, !remove_role")
+                   .AddField("Text Commands", "!compliment, !insult, !quote, !respectcounter")
+                   .AddField("Memes", "!cat, !dankmeme, !surrealmeme")
+                   .WithColor(Color.Orange);                                                                                                                                        //Set color to orange
 
-                .WithColor(Color.Orange);                       //Set color to orange
+
+
             await ReplyAsync("", false, builder.Build());       //Print EmbedBuilder, ReplyAsync takes in string, bool for TTS t/f, EmbedBuillder
 
         }
@@ -81,5 +76,59 @@ namespace ConsoleApp1.Modules
 
         }
 
+        [Command("compliment")] // Second word of command
+        public async Task compliment_help()
+        {
+            //Set up Embed Builder
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.AddField("OrangeBot Help: Compliment", "OrangeBot tries to boosts your confidence.")                                                             //Added Field to builder.  Title, Description
+                   .WithColor(Color.Orange);                                                                                                                    //set color to orange
+            await ReplyAsync("", false, builder.Build());     //Print EmbedBuilder, ReplyAsync takes in string, bool for TTS t/f, EmbedBuillder   
+
+        }
+
+        [Command("dankmeme")] // Second word of command
+        public async Task dankmeme_help()
+        {
+            //Set up Embed Builder
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.AddField("OrangeBot Help: Dankmeme", "OrangeBot sends you the dankest of memes.")                                                             //Added Field to builder.  Title, Description
+                   .WithColor(Color.Orange);                                                                                                                    //set color to orange
+            await ReplyAsync("", false, builder.Build());     //Print EmbedBuilder, ReplyAsync takes in string, bool for TTS t/f, EmbedBuillder   
+
+        }
+
+        [Command("cat")] // Second word of command
+        public async Task cat_help()
+        {
+            //Set up Embed Builder
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.AddField("OrangeBot Help: Cat", "OrangeBot sends you some cute cats.")                                                             //Added Field to builder.  Title, Description
+                   .WithColor(Color.Orange);                                                                                                                    //set color to orange
+            await ReplyAsync("", false, builder.Build());     //Print EmbedBuilder, ReplyAsync takes in string, bool for TTS t/f, EmbedBuillder   
+
+        }
+
+        [Command("surrealmeme")] // Second word of command
+        public async Task surrealmeme_help()
+        {
+            //Set up Embed Builder
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.AddField("OrangeBot Help: Surrealmeme", "O̥̙r͔̣̳̫̱̲̠a̼̝ͅņg̗̦͚̠e͎̟͕͈̪̝B̷͉o̧̦̤̦͈t̫̠ ̵̬̙s̶̬̞͕͚͔͇̩e̗͉n͙̫̬͔d͙͈s̱ ̥̪y̙͎̝̪̙̯̥o̰̯ụ̹̥̜ ̮̲̟̦͉̳ͅt̫̫̰͕̙̗͎͘o̠̙̰̫͔̞̮ ͕̟̙͝ͅt͕͔̜͓͢h̘e͏͉ ̙̲̮̜v̺͔̹͡o̧̦̞̘i̳̣̤d͉̬̰̪")                                                             //Added Field to builder.  Title, Description
+                   .WithColor(Color.Orange);                                                                                                                    //set color to orange
+            await ReplyAsync("", false, builder.Build());     //Print EmbedBuilder, ReplyAsync takes in string, bool for TTS t/f, EmbedBuillder   
+
+        }
+
+        [Command("respectcounter")] // Second word of command
+        public async Task respectcounter_help()
+        {
+            //Set up Embed Builder
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.AddField("OrangeBot Help: Respect", "Displays the amount of respects paided")                                                             //Added Field to builder.  Title, Description
+                   .WithColor(Color.Orange);                                                                                                                    //set color to orange
+            await ReplyAsync("", false, builder.Build());     //Print EmbedBuilder, ReplyAsync takes in string, bool for TTS t/f, EmbedBuillder   
+
+        }
     }
 }
---
+++ ConsoleApp1/Modules/Memes/dankmeme.cs
@@ -0,0 +1,48 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Modules
+{
+
+    public class dankmeme : ModuleBase<SocketCommandContext>
+    {
+        [Command("dankmeme")] // second word of command
+        public async Task one()
+        {
+
+            if (Context.Channel.Name == "memes")
+            {
+
+                var rand = new Random();
+                var files = Directory.GetFiles("C:\\Users\\Aidan Johnsotn\\Dropbox\\IFTTT\\reddit\\dankmemes");
+
+
+
+                //Generating random number that favors being closer to 0;
+                int i = rand.Next(files.Length);
+
+                //Printing out random number in console
+                Console.WriteLine(i);
+
+                //Set message to contence of file, convert to type int
+                string message = File.ReadAllText(files[i]);
+
+
+                EmbedBuilder builder = new EmbedBuilder();
+                builder.WithTitle(Path.GetFileNameWithoutExtension(files[i]))
+                       .WithImageUrl(message)
+                       .WithColor(Color.Orange);
+
+
+                await ReplyAsync("", false, builder.Build());
+            }
+            else await ReplyAsync("Please use the channel #memes");
+        }
+    }
+}
