32e666297f2bbe09310de2745e6475a125327ac2
Nyveous <38675660+Nyveous@users.noreply.github.com>
2018-04-23 21:45:13 -0400

    Cyno's Update (added quotes + minor text changes)

--- ConsoleApp1/Modules/quote.cs
+++ ConsoleApp1/Modules/quote.cs
@@ -23,7 +23,7 @@ namespace ConsoleApp1.Modules
             string[] freshquote; // Create array of string
             freshquote = new string[]
             {
-                    "\"I'm poping off\" - DG06",
+                    "\"I'm popping off\" - DG06",
                     "\"Skate fast eat ass\" - OrangeLube",
                     "\"I am a person who isn't bald for sure.  100% certain\" - Enum",
                     "\"I have a ball over here for you\" - Orange",
@@ -31,12 +31,13 @@ namespace ConsoleApp1.Modules
                     "\"You just have to not be a dumbass\" - Enum",
                     "\"I spent 60 dollars on this game and I'm not allowed to play it\" - Sunny78",
                     "\"Can you sub for our next 20 games\" - Gash",
-                    "\"Did someone say peanutbutter\" - Cynosure", 
-                    "\"Art major BTW\" - Cynosure",
+                    "\"Did someone say peanut butter\" - Cynosure", 
+                    "\"Music major BTW\" - Cynosure",
+                    "\"Psych major BTW\" - Cynosure",
                     "\"Ahhhhhhhhhhhh\" - Cynosure",
                     "\"I'm going to the beach\" - DG06",
-                    "\"I could dunk on you\" -DG06",
-                    "\"RIEN RIEN RIEN\" - Cynosure",
+                    "\"I could dunk on you\" - DG06",
+                    "\"REIN REIN REIN\" - Cynosure",
                     "\"Spear get the fuck back\" - Enum",
                     "\"pls boobs\" - Cynosure",
                     "\"That's real fishy.... \" - Raid team 6",
@@ -52,13 +53,26 @@ namespace ConsoleApp1.Modules
                     "\"I will leave and make a new one, that supports bilingualism and everyone from this server will join and leave this one\" - Martti",
                     "\"You want to talk commie? go to another server\" - Sunny78",
                     "\"I thought mine was big, but then she took off her pants\" - DG06"
+                    "\"I'm not wearing any pants\" - Yukali",
+                    "\"Fight in the courtyard\" - Yukali",
+                    "\"Don't sleep on my boi Profit\" - Sequoia",
+                    "\"I'm no goddamn coward\" - Enum",
+                    "\"I'm retarded but at least I'm not a coward\" - Cynosure",
+                    "\"The enemy has blade. Don't ask me how I know\" - Cynosure",
+                    "\"This quote is brought to you by Sprite\" - Sunny78",
+                    "\"I'm a junkrat one trick\" - DG06",
+                    "\"No aim no brain Winston main btw\" - Cynosure",
+                    "\"You guys blew all your ults on that fight\" - Yukali",
+                    "\"Sorry I have something at 7\" - Cynosure",
+                    "\"Does anyone want to play goodnite with me\" - Spearslim",
+                    "\"rude\" - OrangeLube",
             };
 
             //Random event,  1/20 chance of happening
             int ran = rand.Next(20);
             if (ran == 1)
             {
-                await ReplyAsync($"Damm {Context.User.Username} I'd sure love to gobble up that thicc and thight bootie hole of yours.");
+                await ReplyAsync($"Damm {Context.User.Username} I'd sure love to gobble up that thicc and tight bootie hole of yours.");
                 await Task.Delay(4000);  //Wait 4 seconds
                 await ReplyAsync($"Shit");
                 await Task.Delay(3000);  //Wait 3 seconds
