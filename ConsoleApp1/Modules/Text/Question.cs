ef20ecaa2ece5dafdb3decadad5e425207c338ac
Aidan Johnston <38672170+AidanJohnston@users.noreply.github.com>
2018-04-26 22:09:06 -0400

    Update Question.cs

--- ConsoleApp1/Modules/Text/Question.cs
+++ ConsoleApp1/Modules/Text/Question.cs
@@ -8,25 +8,19 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1.Modules
 {
-     //First word of command
+     
     public class Question : ModuleBase<SocketCommandContext>
     {
         Random rand = new Random();
 
         [Command("ask")] // second word of command
-        public async Task one(string ask)
+        public async Task one([Remainder]string ask)
         {
+        
+            //Generate random 50% change of 1, 2
             int ran = rand.Next(1, 2);
             if (ran == 1) await ReplyAsync("yes");
             if (ran == 2) await ReplyAsync("no");
-
-
-
-
-
         }
-
-        
-
     }
 }
